7acf664436edcca19d9d9f360503e65b2d76dc60
Abdullah Jasim <ajasim@sfu.ca>
2019-06-07 12:51:46 -0700

    More doodles

--- CodeDoodles.cpp
+++ CodeDoodles.cpp
@@ -1,5 +1,10 @@
+//a file for me to test several C++ specific behaviors
+//there will be no attention given to the quality of the code or its structre
+//this is purely for testing specific scenarios and adding my notes to each case
+
 #include <iostream>
 #include <string>
+#include <vector>
 
 //notice the order of inclusions
 #include "LoopRecursionTestOne.h"
@@ -86,6 +91,7 @@ void TestForCtorException() {
 void TestForNewAndDeleteOverload() {
 	class Example {
 	public:
+		//note that this does not call the constructor, new operator does
 		void* operator new(size_t bytes) {
 			std::cout << "The new operator is first called" << std::endl;
 			
@@ -105,6 +111,11 @@ void TestForNewAndDeleteOverload() {
 			std::cout << "When delete is called, the dtor is first called" << std::endl;
 		}
 
+		//note that if new is overloaded, it is impreative that a corresponding delete overload is defined
+		//specifically, if an overloaded new method throws an exception, the corresponding delete will be called
+		//this delete will be called in every other instance
+		//note that this does not call the destructor, delete operator does
+		//if we want to call a specific delete operator, we have to call the dtor manually then
 		void operator delete(void* p) {
 			std::cout << "Finally, the delete operator is called" << std::endl;
 			free(p);
@@ -154,6 +165,38 @@ void TestForNullAgainstVoidPtr() {
 	}
 }
 
+void TestForVectorGrowth() {
+	//std::vector will utilize a move constructor during a reallocation only if that move 
+	//constructor is declared with a noexcept specifier, or if there is no available copy 
+	//constructor
+
+	class Example {
+	public:
+		Example() { std::cout << "Ctor called" << std::endl; };
+		Example(const Example& src) { std::cout << "Copy ctor called" << std::endl; };
+		//Example(const Example&& src) { std::cout << "This move ctor will not be called" << std::endl; };
+		Example(const Example&& src) noexcept { std::cout << "Move ctor called" << std::endl; };
+	};
+
+	std::vector<Example> exs;
+
+	//will call ctor
+	Example ex1;
+
+	//will call copy ctor
+	exs.push_back(ex1);
+
+	//will call ctor
+	Example ex2;
+
+	//will call copy ctor on ex2, and move ctor on ex1 as the vector will grow dynamically
+	exs.push_back(ex2);
+
+	//will call ctor on ex3
+	//will then call moe ctor on ex1, ex2 and ex3 as the vector grows
+	exs.emplace_back(Example());
+}
+
 //--------------------------------------------------------------------//
 //----------------------------------MAIN------------------------------//
 //--------------------------------------------------------------------//
@@ -171,9 +214,11 @@ int main() {
 
 	//TestForNewAndDeleteOverload();
 
-	TestForNullAgainstNullptr();
-	TestForNullptrAgainstVoidPtr();
-	TestForNullAgainstVoidPtr();
+	//TestForNullAgainstNullptr();
+	//TestForNullptrAgainstVoidPtr();
+	//TestForNullAgainstVoidPtr();
+
+	TestForVectorGrowth();
 
 	std::cin.get();
 }
